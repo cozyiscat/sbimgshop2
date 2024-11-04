fd178c787a530e3053e2f5623ba9e9e4af3da06b
cozyiscat <yuiop7664@gmail.com>
2024-11-04 16:29:06 +0900

    그룹코드 등록시 유효성 오류를 각 필드에 표시

--- src/main/resources/static/js/app/code-group.js
+++ src/main/resources/static/js/app/code-group.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
 	console.log("codeGroupRead")		
 	})
 	$("#codeGroupRegisterId").click(function(){
+		//이전 에러 표시 초기화
+		clearErrors();
+		
 		const codeGroupObj = {
 			groupCode: $("#groupCodeId").val(),
 			groupName: $("#groupNameId").val(),
@@ -22,12 +25,24 @@ $(document).ready(function(){
 			},
 			body: JSON.stringify(codeGroupObj)
 		})
-		.then(response => response.json())
+		.then(response => {
+			if(!response.ok){
+				return response.json().then(errorData => {
+					throw errorData;
+				})
+			}
+			response.json()
+		})
 		.then(data => console.log(data))
-		.catch(error => console.error('Error:', error));	
+		.catch(error => {
+			if(error.errors){
+			console.error('Error:', error);				
+			} else {
+			//일반적인 에러 처리	
+			alert(error.message || '처리 중 오류가 발생했습니다.');
+			}	
+		});
 		
-		alert("등록됨");
-	
 	})
 	$("#codeGroupDeleteId").click(function(){
 	console.log("codeGroupDelete")		
