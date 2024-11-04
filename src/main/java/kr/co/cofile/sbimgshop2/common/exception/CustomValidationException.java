17e298f011e30613757e1b745776f250b5e2f31c
cozyiscat <yuiop7664@gmail.com>
2024-11-04 15:43:18 +0900

    예외처리- 유효성검사

---
+++ src/main/java/kr/co/cofile/sbimgshop2/common/exception/CustomValidationException.java
@@ -0,0 +1,15 @@
+package kr.co.cofile.sbimgshop2.common.exception;
+
+import org.springframework.validation.BindingResult;
+
+import lombok.Getter;
+
+@Getter
+public class CustomValidationException extends BusinessException {
+	private final BindingResult bindingResult;
+	
+	public CustomValidationException(ErrorCode errorCode, BindingResult bindingResult) {
+		super(errorCode);
+		this.bindingResult = bindingResult;
+	}
+}
--- src/main/java/kr/co/cofile/sbimgshop2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/cofile/sbimgshop2/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package kr.co.cofile.sbimgshop2.common.exception;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +22,19 @@ public class GlobalExceptionHandler {
 		log.error("Business Exception",e);
 		
 		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(CustomValidationException.class)
+	protected ResponseEntity<ErrorResponse>
+	handleValidationException(CustomValidationException e){
+		log.error("validation Exception", e);
+		
+		BindingResult bindingResult = e.getBindingResult();
+		Map<String, String> errors =
+				ErrorResponse.bindingResultToMap(bindingResult);
+		
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode(),errors);
 		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 	
