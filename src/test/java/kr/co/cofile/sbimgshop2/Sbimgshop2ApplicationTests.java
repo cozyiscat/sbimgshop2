99d793400f06108308a077522673f1519e6a7aa2
cozyiscat <yuiop7664@gmail.com>
2024-11-04 11:40:59 +0900

    프로젝트 초기화

---
+++ src/test/java/kr/co/cofile/sbimgshop2/Sbimgshop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbimgshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbimgshop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
